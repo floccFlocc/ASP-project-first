317c274a6c0d9f372ab2d38f43943b431a6ba456
Andrei Palma <andrei.palma2000@gmail.com>
2023-09-18 21:16:47 +0800

    Add project files.

---
+++ ASP-project-first/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_project_first.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
